b088782d06246d6c68e7d8a5208f7615a2b4b848
Haile <haile4cmd@gmail.com>
2021-06-16 19:53:55 +0300

    added the javascript fache function

---
+++ js/javas.js
@@ -0,0 +1,21 @@
+const jokeEL = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokebtn')
+
+jokeBtn.addEventListener('click',generatejoke)
+
+generatejoke()
+
+function generatejoke(){
+    const config ={
+        headers: {
+            Accept: 'application/json',
+        },
+  }
+
+      fetch('https://icanhazdadjoke.com' , config)
+      .then((res) => res.json())
+      .then((data)=>{
+          jokeEL.innerHTML = data.joke
+      })
+    
+}
